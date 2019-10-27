661d89999bbd029f59285a03b1cb91169c66f8a9
Raphael Agbozo <raphaelkwesi@gmail.com>
2019-10-27 21:44:56 +0000

    All tests passed2

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,7 +36,7 @@ public class InfoActivity extends AppCompatActivity {
 
     public void createPhoneIntent(View view) {
         Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse("tel:0123456789"));
+        intent.setData(Uri.parse("tel:01234567890"));
         startActivity(intent);
     }
 
